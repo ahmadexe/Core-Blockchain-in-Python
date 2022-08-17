fedc6ca3fc9bb56f761393c779135611cbd71f94
Ahmad <muahmad710@gmail.com>
2022-08-17 05:27:51 +0500

    Generating chain and genesis block

--- blockchain.py
+++ blockchain.py
@@ -4,4 +4,10 @@ import json
 import flask
 from flask import Flask, jsonify
 
-
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {}
