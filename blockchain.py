d8c684f97db56758e2ff75e824ae9ef3936a1ace
Ahmad <muahmad710@gmail.com>
2022-08-17 07:13:21 +0500

    Chain validation added

--- blockchain.py
+++ blockchain.py
@@ -31,3 +31,25 @@ class Blockchain:
         else:
             new_proof += 1
         return new_proof
+    
+    def hash_block(self, block):
+        encoded_block = json.dump(block, sort_keys= True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash_block(previous_block):
+                return False
+            proof = block['proof']
+            previous_proof = previous_block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+            
