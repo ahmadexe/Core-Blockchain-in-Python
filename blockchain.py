bef28bd1a545c6e4855a853cb933f4655aefc7fe
Ahmad <muahmad710@gmail.com>
2022-08-17 06:04:21 +0500

    Proof of work function done

--- blockchain.py
+++ blockchain.py
@@ -5,9 +5,29 @@ import flask
 from flask import Flask, jsonify
 
 class Blockchain:
+    
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
         
     def create_block(self, proof, previous_hash):
-        block = {}
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+        if hash_operation[:4] == '0000':
+            check_proof = True
+        else:
+            new_proof += 1
+        return new_proof
